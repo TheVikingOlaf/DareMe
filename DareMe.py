0a145f8438aa2162f529d1948365d58b3431de13
aiyion.prime <git@aiyionpri.me>
2019-11-01 23:20:16 +0100

    DareMe.py: make use of Challenges

--- DareMe.py
+++ DareMe.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
+from os.path import expanduser
 
 import RPi.GPIO as GPIO
+from challenge import ChallengeStack
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
@@ -19,7 +21,13 @@ def coin_added(value):
 
 GPIO.add_event_callback(4, coin_added)
 
+home = expanduser("~")
+cf = home+"/challenges"
+stack = ChallengeStack.from_folder(cf)
+print(stack)
+
 while True:
     if coinInserted:
         coinInserted = False
         print("Coin inserted. New credit: {0:.2f}€".format(coinsValue / 100))
+        print("{}: {}".format(stack.passed, stack.pick()))
