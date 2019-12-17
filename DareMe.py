415356b9014e0d2d7fc33bb3736225c504bdb069
g3n35i5 <janfschmidt@mailbox.org>
2019-12-17 18:07:51 +0100

    Possible fix for the "two challenge problem"

--- DareMe.py
+++ DareMe.py
@@ -4,6 +4,7 @@ from DareFormatter import Dare
 from os.path import expanduser
 import RPi.GPIO as GPIO
 from thermalprinter import *
+import datetime
 
 coin_pin = 4
 
@@ -13,7 +14,20 @@ GPIO.setup(coin_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 coinsValue: float = 0.00
 coinInserted: bool = False
 
+last_timestamp = datetime.datetime.utcnow()
+GUARD_INTERVAL_IN_SECONDS = 5
+
 GPIO.add_event_detect(coin_pin, GPIO.RISING)
+
+
+def guard_interval_passed(timestamp: datetime.datetime) -> bool:
+    if timestamp is None:
+        return True
+    now = datetime.datetime.utcnow()
+    delta = now - timestamp
+    if delta.total_seconds() > GUARD_INTERVAL_IN_SECONDS:
+        return True
+    return False
 
 
 def coin_added(value):
@@ -34,10 +48,11 @@ d = Dare.from_file(home + "/dare.txt")
 
 with ThermalPrinter(port='/dev/serial0') as printer:
     while True:
-        if coinInserted:
-            coinInserted = False
+        if coinInserted and guard_interval_passed(last_timestamp):
             print("Coin inserted. New credit: {0:.2f}€".format(coinsValue / 100))
             msg = d.compile(stack.pick(), stack.cur_count)
             print(msg)
             printer.out(msg, justify='C', strike=True)
             printer.feed(2)
+            last_timestamp = datetime.datetime.utcnow()
+            coinInserted = False
